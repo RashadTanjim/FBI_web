39444fd5890f6ff1991b1e22fdc638cd82bf093f
RashadTanjim <rashad.tanjim@northsouth.edu>
2020-03-28 00:48:11 +0600

    fbi_app created!
    'fbi_app.apps.FbiAppConfig',

--- fbi/fbi/settings.py
+++ fbi/fbi/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fbi_app.apps.FbiAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ fbi/fbi_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FbiAppConfig(AppConfig):
+    name = 'fbi_app'
